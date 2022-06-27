60c5739e86760284328ec02e6cf93fa23fdadc83
Maneesh-Arora <maneesh2000@yahoo.com>
2022-06-27 23:15:48 +0530

    Added JMSCOnfig and HelloSender to send messages periodically

---
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgjms.sender;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+    @Scheduled(fixedRate =  2000)
+    public void sendMessage(){
+        System.out.println("Im sending a message");
+        HelloWorldMessage helloWorldMessage = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello World")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE,helloWorldMessage);
+        System.out.println("Message Sent");
+    }
+}
